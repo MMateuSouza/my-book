3fa978849ff0e0fe4744ac3f15ad4a98627edac0
Mateus Souza <mota.mateus13@gmail.com>
2021-10-10 15:38:48 -0400

    Adicionando classes modelos de livro, autores e capítulos

--- audiobooks/models.py
+++ audiobooks/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 
-class Chapter(models.Model):
-    pass
-
-
-# TODO: Verificar quais campos serão adicionados para instância Livro
-class Book(models.Model):
-    title = None
-    publishing_company = None
-    authors = None
-    edition = None
-    isbn_10 = None
-    isbn_13 = None
-    chapters = None
-
-
 # TODO: Verificar quais campos serão adicionados para instância AudioBook
-class AudioBook(models.Model):
-    pass
+# class AudioBook(models.Model):
+#     pass
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name='Título', max_length=255)
+    publishing_company = models.CharField(verbose_name='Editora', max_length=64)
+    edition = models.PositiveIntegerField(verbose_name='Edição')
+    isbn_10 = models.CharField(verbose_name='ISBN 10', max_length=10)
+    isbn_13 = models.CharField(verbose_name='ISBN 13', max_length=10)
+
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(verbose_name='Livro', to='audiobooks.Book', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Autor', max_length=255)
+
+
+class Chapter(models.Model):
+    book = models.ForeignKey(verbose_name='Livro', to='audiobooks.Book', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Título', max_length=255)
+    subchapters = models.ManyToManyField(verbose_name='Subcapítulos', to='self', blank=True)
