1d9fea0021085329bfbb741a3644c65609cfbadd
Mateus Souza <mota.mateus13@gmail.com>
2021-10-22 22:50:54 -0400

    Adicionando javascript para feedback no cadastro de usuário

--- static/js/users/create.js
+++ static/js/users/create.js
@@ -1,1 +1,15 @@
-(function() {})();
+(function() {
+  let isNewUser = document.querySelector('#is-new-user').checked
+  let queryElementCondition = isNewUser ? 'input.form-control' : 'input.form-control:not([type=password])' ;
+
+  document.querySelectorAll(queryElementCondition).forEach((el) => {
+    el.addEventListener('input', (e) => {
+      let target = e.target;
+      let targetValue = target.value;
+      let isInvalid = target.classList.contains('is-invalid');
+
+      !targetValue && !isInvalid && target.classList.add('is-invalid');
+      targetValue && isInvalid && target.classList.remove('is-invalid');
+    })
+  });
+})();
