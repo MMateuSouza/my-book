1bc5fad353518d9f18fb236415b700b970be92c4
Mateus Souza <mota.mateus13@gmail.com>
2021-11-04 18:39:49 -0400

    Adicionando validação no JS para arquivos de áudio

---
+++ static/js/audiobooks/recording.js
@@ -0,0 +1,32 @@
+(function () {
+  let formElement = document.querySelector('#audiobook_form');
+  let submitFormButton = document.querySelector('#submit_form');
+  let fileInputElements = document.querySelectorAll('input[type="file"]');
+
+  submitFormButton.onclick = (e) => validateFormBeforeSubmit(e);
+
+  fileInputElements.forEach(function(el) {
+    el.oninput = function() {
+      let targetValue = el.value;
+      let isInvalid = el.classList.contains('is-invalid');
+
+      !targetValue && !isInvalid && el.classList.add('is-invalid');
+      targetValue && isInvalid && el.classList.remove('is-invalid');
+    }
+  });
+
+  function validateFormBeforeSubmit(e) {
+    e.preventDefault();
+    let isValid = true;
+
+    // TODO: Implementar verificação dos inputs
+    fileInputElements.forEach(function(el) {
+      if (!el.value) {
+        isValid = false;
+        el.classList.add('is-invalid');
+      }
+    });
+
+    isValid && formElement.submit();
+  }
+})();
