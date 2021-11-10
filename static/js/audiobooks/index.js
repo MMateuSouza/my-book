4dc1290596c288ff6c1b8fb760b94d2dc1dcdd45
Mateus Souza <mota.mateus13@gmail.com>
2021-11-10 10:51:09 -0400

    Adicionando status ativo na barra de navegação

---
+++ static/js/audiobooks/index.js
@@ -0,0 +1,3 @@
+(function () {
+  document.querySelector('#audiobooks').classList.add('active');
+})();
---
+++ static/js/main/index.js
@@ -0,0 +1,3 @@
+(function () {
+  document.querySelector('#main').classList.add('active');
+})();
